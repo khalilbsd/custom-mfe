dce8da3fbfb337db51e6510c88f7ab51db8ffeb1
khalilbsd <78567534+khalilbsd@users.noreply.github.com>
2022-05-18 10:34:46 +0100

    changing paypal image

--- src/payment/payment-methods/paypal/PayPalButton.jsx
+++ src/payment/payment-methods/paypal/PayPalButton.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
-
-import PayPalLogo from './assets/paypal-logo.png';
-import messages from './PayPalButton.messages';
+import React from "react";
+import PropTypes from "prop-types";
+import { injectIntl, intlShape } from "@edx/frontend-platform/i18n";
+import AcceptedCardLogos from '../../checkout/assets/accepted-card-logos.png';
+import PayPalLogo from "./assets/paypal-logo.png";
+import messages from "./PayPalButton.messages";
 
 const PayPalButton = ({ intl, isProcessing, ...props }) => (
   <button type="button" {...props}>
-    { isProcessing ? <span className="button-spinner-icon text-primary mr-2" /> : null }
+    {isProcessing ? (
+      <span className="button-spinner-icon text-primary mr-2" />
+    ) : null}
     <img
-      src={PayPalLogo}
-      alt={intl.formatMessage(messages['payment.type.paypal'])}
+      src={AcceptedCardLogos}
+      alt={intl.formatMessage(messages["payment.page.method.type.credit"])}
     />
   </button>
 );
