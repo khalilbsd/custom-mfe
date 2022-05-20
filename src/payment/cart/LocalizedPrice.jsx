b468d73db251fbf0b7001fa4fe2eb4a6874200cc
khalilbsd <78567534+khalilbsd@users.noreply.github.com>
2022-05-20 14:12:58 +0100

    chaning default crurrency

--- src/payment/cart/LocalizedPrice.jsx
+++ src/payment/cart/LocalizedPrice.jsx
@@ -55,7 +55,7 @@ LocalizedPrice.propTypes = {
 LocalizedPrice.defaultProps = {
   amount: undefined,
   conversionRate: 1,
-  currencyCode: 'USD',
+  currencyCode: 'TND',
   showAsLocalizedCurrency: false,
 };
 
