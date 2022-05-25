e04b3253069fabfde0e58148bf8d8c31737db00a
khalilbsd <78567534+khalilbsd@users.noreply.github.com>
2022-05-25 12:15:17 +0100

    adding new form

---
+++ src/payment/checkout/CustomForm.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { PayPalButton } from '../payment-methods/paypal';
+import TextField from '@material-ui/core/TextField';
+class CustomForm extends React.Component {
+    
+    render() { 
+        return (
+           <>
+                <TextField onChange={this.props.handleChange} label="First name" type="text" id="outlined-basic" name="name" placeholder="name"  alt="name" variant="outlined" required />
+                <TextField onChange={this.props.handleChange} label="Last name" type="text" id="lastName" name="lastName" placeholder="last name" alt="last name" variant="outlined" required />
+                <TextField onChange={this.props.handleChange} label="Adress" type="text" name="adress" id="addr" alt="Adress" placeholder="Adress" variant="outlined" />
+                <TextField onChange={this.props.handleChange} label="Adress" type="text" name="tel" id="tel" alt="tel" placeholder="tel" variant="outlined" /> 
+           </>   
+        );
+    }
+}
+ 
+export default CustomForm ;
